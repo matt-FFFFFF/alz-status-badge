82b49e8591532a8ad347a9bde4bd93e5db038811
Matt White <matt.white@microsoft.com>
2022-04-30 15:31:11 +0100

    Big update

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 
 	svg "github.com/ajstarks/svgo"
 )
 
+// main starts the server.
 func main() {
 	listenAddress := ":8080"
 	http.HandleFunc("/api/badge", badge)
@@ -14,15 +17,74 @@ func main() {
 	log.Fatal(http.ListenAndServe(listenAddress, nil))
 }
 
+// badge returns the badge for the given status.
 func badge(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Printing headers:")
-	for k, v := range r.Header {
-		log.Printf("%s: %s", k, v)
+	log.Printf("Received request from: %s (%s)", r.RemoteAddr, r.Header["X-Request-Id"])
+
+	variant := r.URL.Query().Get("variant")
+	if variant == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Missing variant parameter."))
+		log.Printf("Missing variant parameter (%s)", r.Header["X-Request-Id"])
+		return
 	}
-	log.Printf("Remote addr: %s", r.RemoteAddr)
+
+	re := regexp.MustCompile(`^\w{1,32}$`)
+
+	if ok := re.Match([]byte(variant)); !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(fmt.Sprintf("Invalid variant parameter '%s'. Must be 32 characters or less. a-z, A-Z, 0-9 only.", variant)))
+		log.Printf("Invalid variant parameter '%s' (%s)", variant, r.Header["X-Request-Id"])
+		return
+	}
+
 	w.Header().Set("Content-Type", "image/svg+xml")
 	s := svg.New(w)
-	s.Start(250, 100)
-	s.Text(10, 20, "Valid!", "font-family=\"Verdana\" font-size=\"12\" fill=\"blue\"")
+
+	approved := checkVariant(variant)
+	log.Printf("Variant: %s approval is %t (%s)", variant, approved, r.Header["X-Request-Id"])
+	makeBadge(variant, approved, s)
+	log.Printf("End request from: %s (%s)", r.RemoteAddr, r.Header["X-Request-Id"])
+}
+
+// makeBadge creates a badge for the given status.
+func makeBadge(text string, approved bool, s *svg.SVG) {
+	var rightFill string
+	switch approved {
+	case true:
+		rightFill = "#4c1"
+	case false:
+		rightFill = "#c41"
+	}
+
+	w := 200
+	h := 20
+	split := 0.35
+	greenRectX := int(float64(w) * split)
+	greenRectW := w - greenRectX
+	pathStyle := fmt.Sprintf("M%d 0h4v20h-4z", greenRectX)
+	leftTextX := greenRectX / 2
+	rightTextX := ((w - greenRectX) / 2) + greenRectX
+	sc := []svg.Offcolor{
+		{Offset: 0, Color: "#bbb", Opacity: 0.1},
+		{Offset: 100, Opacity: 0.1},
+	}
+	s.Start(w, h)
+	s.LinearGradient("g", 0, 0, 0, uint8(w), sc)
+	s.Rect(0, 0, w, h, "fill=\"#555\" rx=\"3\"")
+	s.Rect(greenRectX, 0, greenRectW, h, fmt.Sprintf("fill=\"%s\" rx=\"3\"", rightFill)) // This is the green rectangle
+	s.Path(pathStyle, fmt.Sprintf("fill=\"%s\"", rightFill))
+	s.Rect(0, 0, w, h, "fill=\"url(#g)\" rx=\"3\"")
+	s.Gstyle("fill: rgb(255, 255, 255);	text-anchor: middle; font-family: DejaVu Sans, Verdana, Geneva, sans-serif;	font-size: 11;")
+	s.Text(leftTextX, 15, "alz-variant", "fill=\"#010101\"	fill-opacity=\"0.3\"")
+	s.Text(leftTextX, 14, "alz-variant")
+	s.Text(rightTextX, 15, text, "fill=\"#010101\"	fill-opacity=\"0.3\"")
+	s.Text(rightTextX, 14, text)
+	s.Gend()
 	s.End()
+}
+
+// checkVariant returns the status of the given variant.
+func checkVariant(variant string) bool {
+	return variant == "canadapubsec"
 }
