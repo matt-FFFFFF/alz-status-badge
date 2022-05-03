35d6b8ff9ad7d246548fc3f56caad808ee5b4674
Matt White <matt.white@microsoft.com>
2022-05-03 13:04:02 +0100

    Update variants

---
+++ approvedvariants.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+func updateApprovedVariants(av ApprovedVariants, url string) {
+	for {
+		log.Printf("Checking approved variants")
+		vl, err := getApprovedVariants(&url)
+		if err != nil {
+			log.Printf("Error downloading approved variants: %s", err.Error())
+		}
+
+		for _, v := range vl {
+			av[v] = nil
+		}
+
+		var vs string
+		for v := range av {
+			vs = vs + (fmt.Sprintf("%s, ", v))
+		}
+
+		log.Printf("Approved variants: %s", vs)
+		time.Sleep(time.Second * 15)
+	}
+}
+
+func getApprovedVariants(url *string) ([]string, error) {
+	resp, err := http.Get(*url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	v, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	vl := make([]string, 0)
+	err = json.Unmarshal(v, &vl)
+	if err != nil {
+		return nil, err
+	}
+
+	return vl, nil
+}
